166a466a24226e3e4d4b325d51a99a46e4dbd542
Eivind Kristoffersen <eivindskn@hotmail.com>
2017-01-25 03:26:24 +0100

    Update map_world_region.js

--- app/assets/javascripts/map_world_region.js
+++ app/assets/javascripts/map_world_region.js
@@ -38,7 +38,7 @@ function initialize1() {
 	var mapOptions = {
 		disableDefaultUI : false,
 
-		zoom : 2,
+		zoom : 3,
 
 		mapTypeControlOptions : {
 			mapTypeIds : [google.maps.MapTypeId.SATELLITE, MY_MAPTYPE_ID]
